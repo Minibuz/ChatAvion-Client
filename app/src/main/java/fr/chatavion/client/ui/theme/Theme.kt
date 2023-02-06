15ccbe11ca776a98666e46e0c4ed2dd6328d4f86
Léo Barroux <leo.barroux@gmail.com>
2023-02-06 15:54:06 +0100

    feat: first version of application theme (colors + police)

--- app/src/main/java/fr/chatavion/client/ui/theme/Theme.kt
+++ app/src/main/java/fr/chatavion/client/ui/theme/Theme.kt
@@ -7,25 +7,28 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = Black,
+    primary = Black,
+    onPrimary = White,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    background = White,
+    primary = White,
+    onPrimary = Black,
+    onBackground = Black,
+    onSurface = Black
 )
+
+/* Other colors to overwrite :
+    primaryVariant,
+    secondary,
+    surface,
+    onSecondary,
+    ...
+ */
 
 @Composable
 fun ChatavionTheme(
--- app/src/main/java/fr/chatavion/client/ui/theme/Type.kt
+++ app/src/main/java/fr/chatavion/client/ui/theme/Type.kt
@@ -2,14 +2,16 @@ package fr.chatavion.client.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import fr.chatavion.client.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.liberationsans_regular)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
