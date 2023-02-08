d38beb0aa760e7740d7637a45df05d297b77daa3
Léo Barroux <leo.barroux@gmail.com>
2023-02-08 10:09:10 +0100

    feat: added verification, colors, placeholders

--- app/src/main/java/fr/chatavion/client/ui/theme/Color.kt
+++ app/src/main/java/fr/chatavion/client/ui/theme/Color.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Black = Color(0xFF000000)
 val BlackBackground =  Color(0XFF0B080F)
 val Gray = Color(0xFFADADAD)
+val LightGray = Color(0xFFCFCECE)
 val Blue = Color(0XFF38B6FF)
 val White = Color(0XFFFFFFFF)
 val Red = Color(0XFFFF0000)
