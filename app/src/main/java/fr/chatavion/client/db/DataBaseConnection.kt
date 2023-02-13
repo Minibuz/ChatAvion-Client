fbc46b47f19fed3c28fc53b6fd55c793034e0178
Léo Barroux <leo.barroux@gmail.com>
2023-02-13 16:42:03 +0100

    feat: init db room

--- app/src/main/java/fr/chatavion/client/db/DataBaseConnection.kt
+++ app/src/main/java/fr/chatavion/client/db/DataBaseConnection.kt
@@ -1,2 +1,36 @@
 package fr.chatavion.client.db
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    version = 1,
+//    entities = [Plant::class]
+)
+
+//@TypeConverters(Converters::class)
+abstract class DataBaseConnection : RoomDatabase() {
+//    abstract fun plantDao(): PlantDAO
+
+    companion object {
+        // For Singleton instantiation
+        @Volatile
+        private var instance: DataBaseConnection? = null
+
+        fun getInstance(context: Context): DataBaseConnection {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): DataBaseConnection {
+
+            return Room.databaseBuilder(
+                context.applicationContext,
+                DataBaseConnection::class.java, "DB"
+            ).build()
+        }
+    }
+}
