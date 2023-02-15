dc275bf66fb0cb0abb3c5dfd232e9ef9c481dfa0
Léo Barroux <leo.barroux@gmail.com>
2023-02-15 15:39:41 +0100

    fix: sender as global

--- app/src/main/java/fr/chatavion/client/ui/view/AuthentificationView.kt
+++ app/src/main/java/fr/chatavion/client/ui/view/AuthentificationView.kt
@@ -27,11 +27,11 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 
 class AuthentificationView {
+    private val sender = DnsResolver()
 
     @Composable
     @SuppressLint("NotConstructor")
     fun AuthentificationView(navController: NavController) {
-        val sender = DnsResolver()
         val context = LocalContext.current
         var id by remember { mutableStateOf("") }
         var pseudo by remember { mutableStateOf("") }
@@ -108,7 +108,7 @@ class AuthentificationView {
                                 Log.i("Community", community)
                                 Log.i("Address", address)
                                 CoroutineScope(IO).launch {
-                                    isConnectionOk = sendButtonConnexion(sender, address, community)
+                                    isConnectionOk = sendButtonConnexion(address, community)
                                 }
                             } else {
                                 showToast(
@@ -138,7 +138,6 @@ class AuthentificationView {
     }
 
     private suspend fun sendButtonConnexion(
-        sender: DnsResolver,
         address: String,
         community: String
     ): Boolean {
