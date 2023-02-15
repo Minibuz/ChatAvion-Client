3dd6918bfa5ebf24ba49e6972ee2c9e8cdb61a89
Dreccuss <martin.ricard@edu.univ-eiffel.fr>
2023-02-15 16:58:04 +0100

    Feat : initialized first automatized tests (connection to default@chatavion.com as and sending "message")

--- app/src/test/java/fr/chatavion/client/appium/index.js
+++ app/src/test/java/fr/chatavion/client/appium/index.js
@@ -8,21 +8,48 @@ const opts = {
   port: 4723,
   capabilities: {
     platformName: "Android",
-    platformVersion: "8.1",
-    deviceName: "advtest",
-    appPackage: "fr.chatavion.client",
-    appActivity: ".MainActivity",
-    automationName: "UiAutomator2"
+    platformVersion: "8.1",             //ANDROID VERSION OF EMULATOR
+    deviceName: "advtest",              //NAME OF YOUR EMULATOR IN ANDROID STUDIO (in device manager)
+    appPackage: "fr.chatavion.client",  //PACKAGE IN WHICH ACTIVITY IS IN
+    appActivity: ".MainActivity",       //NAME OF TO BE TESTED ACTIVITY
+    automationName: "UiAutomator2",
+    disableIdLocatorAutocompletion: "true"
   }
 };
 
+
+
+
 async function main () {
-  const client = await wdio.remote(opts);
-    const field = await client.$("android.widget.EditText");
-    await field.setValue("Hello World!");
-    const value = await field.getText();
-    assert.strictEqual(value, "Hello World!");
-  await client.deleteSession();
+
+  const driver = await wdio.remote(opts);
+
+    //id communaute
+    let selector = 'new UiSelector().resourceId("textEditCommu")';
+    const fieeeld = await driver.$(`android=${selector}`)
+    await fieeeld.setValue("default@chatavion.com")
+
+    //Pseudo
+    selector = 'new UiSelector().resourceId("textEditPwd")';
+    const pssd = await driver.$(`android=${selector}`)
+    await pssd.setValue("user");
+
+    //bouton Rejoindre
+    selector = 'new UiSelector().resourceId("connectionBtn")';
+    const connect = await driver.$(`android=${selector}`)
+    await connect.click();
+
+    //bottom bar message text field
+    selector = 'new UiSelector().resourceId("msgEditField")';
+    const msg = await driver.$(`android=${selector}`)
+    await msg.setValue("message");
+
+    //send button
+    selector = 'new UiSelector().resourceId("sendBtn")';
+    const send = await driver.$(`android=${selector}`)
+    await send.click();
+
+  await driver.deleteSession();
 }
 
 main();
