abfcb6333f11813607e33fd53e304fed39748817
Léo Barroux <leo.barroux@gmail.com>
2023-02-16 10:23:45 +0100

    feat: Authentification Page save pseudo in SettingsRepository

---
+++ app/src/androidTest/java/fr/chatavion/client/SettingsRepositoryTest.kt
@@ -0,0 +1,88 @@
+package fr.chatavion.client
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import fr.chatavion.client.datastore.SettingsRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class SettingsRepositoryTest {
+
+    private lateinit var context: Context
+    private lateinit var repository: SettingsRepository
+
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext()
+        repository = SettingsRepository(context)
+    }
+
+    @Test
+    fun testGetAndSetTheme() = runBlocking {
+        val testTheme = SettingsRepository.Theme.Dark
+        repository.setTheme(testTheme)
+        val theme = repository.theme.first()
+        assertEquals(testTheme, theme)
+    }
+
+    @Test
+    fun testGetAndSetPseudo() = runBlocking {
+        val testPseudo = "Test User"
+        repository.setPseudo(testPseudo)
+        val pseudo = repository.pseudo.first()
+        assertEquals(testPseudo, pseudo)
+    }
+
+    @Test
+    fun testGetAndSetDnsTypeTransaction() = runBlocking {
+        val testDnsTypeTransaction = "Test DNS Type Transaction"
+        repository.setDnsTypeTransaction(testDnsTypeTransaction)
+        val dnsTypeTransaction = repository.dnsTypeTransaction.first()
+        assertEquals(testDnsTypeTransaction, dnsTypeTransaction)
+    }
+
+    @Test
+    fun testGetAndSetProtocolChoice() = runBlocking {
+        val testProtocolChoice = SettingsRepository.Protocol.Http
+        repository.setProtocolChoice(testProtocolChoice)
+        val protocolChoice = repository.protocolChoice.first()
+        assertEquals(testProtocolChoice, protocolChoice)
+    }
+
+    @Test
+    fun testGetAndSetLanguage() = runBlocking {
+        val testLanguage = SettingsRepository.Language.English
+        repository.setLanguage(testLanguage)
+        val language = repository.language.first()
+        assertEquals(testLanguage, language)
+    }
+
+    @Test
+    fun testGetAndSetRefreshTime() = runBlocking {
+        val testRefreshTime = 60L
+        repository.setRefreshTime(testRefreshTime)
+        val refreshTime = repository.refreshTime.first()
+        assertEquals(testRefreshTime, refreshTime)
+    }
+
+    @Test
+    fun testGetAndSetHistoryLoading() = runBlocking {
+        val testHistoryLoading = 10
+        repository.setHistoryLoading(testHistoryLoading)
+        val historyLoading = repository.historyLoading.first()
+        assertEquals(testHistoryLoading, historyLoading)
+    }
+
+    @Test
+    fun testGetAndSetEncoding() = runBlocking {
+        val testEncoding = "Test Encoding"
+        repository.setEncoding(testEncoding)
+        val encoding = repository.encoding.first()
+        assertEquals(testEncoding, encoding)
+    }
+
+}
+
--- app/src/main/java/fr/chatavion/client/ui/view/AuthentificationView.kt
+++ app/src/main/java/fr/chatavion/client/ui/view/AuthentificationView.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import fr.chatavion.client.R
 import fr.chatavion.client.connection.dns.DnsResolver
+import fr.chatavion.client.datastore.SettingsRepository
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 
@@ -34,6 +35,7 @@ class AuthentificationView {
     @SuppressLint("NotConstructor")
     fun AuthentificationView(navController: NavController) {
         val context = LocalContext.current
+        val settingsRepository = SettingsRepository(context = context)
         var id by remember { mutableStateOf("") }
         var pseudo by remember { mutableStateOf("") }
         var community by remember { mutableStateOf("") }
@@ -110,6 +112,10 @@ class AuthentificationView {
                                 Log.i("Address", address)
                                 CoroutineScope(IO).launch {
                                     isConnectionOk = sendButtonConnexion(address, community)
+                                    if (isConnectionOk) {
+                                        Log.i("Pseudo", "Setting user pseudo to $pseudo")
+                                        settingsRepository.setPseudo(pseudo)
+                                    }
                                 }
                             } else {
                                 showToast(
