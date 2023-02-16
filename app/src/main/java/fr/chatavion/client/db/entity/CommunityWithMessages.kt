ccaf2aceb024d550afdc5912813d46adf0332d35
Léo Barroux <leo.barroux@gmail.com>
2023-02-16 11:32:08 +0100

    feat: data class CommunityWithMessage

---
+++ app/src/main/java/fr/chatavion/client/db/entity/CommunityWithMessages.kt
@@ -0,0 +1,13 @@
+package fr.chatavion.client.db.entity
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class CommunityWithMessages(
+    @Embedded val community: Community,
+    @Relation(
+        parentColumn = "communityId",
+        entityColumn = "communityId"
+    )
+    val playlists: List<Message>
+)
