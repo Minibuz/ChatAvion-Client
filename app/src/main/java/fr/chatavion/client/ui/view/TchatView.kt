55f0755df9f9d2f73be8c3cfdd81d07037db94b0
Rhamao <arnaudapelbaum@gmail.com>
2023-02-23 12:25:23 +0100

    Deleted useless closeDrawer lambda from DrawerContentComponent & ParametersColumn

--- app/src/main/java/fr/chatavion/client/ui/view/TchatView.kt
+++ app/src/main/java/fr/chatavion/client/ui/view/TchatView.kt
@@ -8,7 +8,6 @@ import androidx.activity.compose.BackHandler
 import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
