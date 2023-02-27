f5979386b30527d4975a31ab684599e35821b627
Léo Barroux <leo.barroux@gmail.com>
2023-02-27 15:41:11 +0100

    feat: code documentation

--- app/src/main/java/fr/chatavion/client/db/DataBaseConnection.kt
+++ app/src/main/java/fr/chatavion/client/db/DataBaseConnection.kt
@@ -7,11 +7,23 @@ import androidx.room.RoomDatabase
 import fr.chatavion.client.db.dao.CommunityDAO
 import fr.chatavion.client.db.entity.Community
 
+/**
+ * A Room Database that serves as the main access point for the application's local database.
+ * Provides a singleton instance of [DataBaseConnection] to access the application's database.
+ *
+ * @constructor Creates an instance of [DataBaseConnection]
+ */
 @Database(
     version = 1,
     entities = [Community::class]
 )
 abstract class DataBaseConnection : RoomDatabase() {
+
+    /**
+     * Provides an abstract function to access the [CommunityDAO] interface.
+     *
+     * @return An instance of [CommunityDAO]
+     */
     abstract fun communityDao(): CommunityDAO
 
     companion object {
@@ -19,14 +31,26 @@ abstract class DataBaseConnection : RoomDatabase() {
         @Volatile
         private var instance: DataBaseConnection? = null
 
+        /**
+         * Returns the singleton instance of [DataBaseConnection]. If the instance doesn't exist,
+         * creates a new instance of [DataBaseConnection].
+         *
+         * @param context The application context
+         * @return The singleton instance of [DataBaseConnection]
+         */
         fun getInstance(context: Context): DataBaseConnection {
             return instance ?: synchronized(this) {
                 instance ?: buildDatabase(context).also { instance = it }
             }
         }
 
+        /**
+         * Builds a new instance of [DataBaseConnection] with the given [context] and database name "DB".
+         *
+         * @param context The application context
+         * @return A new instance of [DataBaseConnection]
+         */
         private fun buildDatabase(context: Context): DataBaseConnection {
-
             return Room.databaseBuilder(
                 context.applicationContext,
                 DataBaseConnection::class.java, "DB"
--- app/src/main/java/fr/chatavion/client/db/viewModel/CommunityViewModel.kt
+++ app/src/main/java/fr/chatavion/client/db/viewModel/CommunityViewModel.kt
@@ -10,28 +10,64 @@ import fr.chatavion.client.db.entity.Community
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 
+/**
+ * View Model class for managing Community entities in the database.
+ *
+ * @param application The application instance.
+ */
 class CommunityViewModel(application: Application) : AndroidViewModel(application) {
+
+    // The DAO instance for Community entities.
     private var communityDao: CommunityDAO =
         DataBaseConnection.getInstance(application).communityDao()
 
+    /**
+     * Returns the ID of the Community entity with the given name and address.
+     *
+     * @param name The name of the Community entity.
+     * @param address The address of the Community entity.
+     * @return The ID of the Community entity.
+     */
     fun getId(name: String, address: String): Int {
         return communityDao.getId(name, address)
     }
 
+    /**
+     * Returns a LiveData object containing the Community entity with the given ID.
+     *
+     * @param id The ID of the Community entity.
+     * @return A LiveData object containing the Community entity.
+     */
     fun getById(id: Int): LiveData<Community> {
         return communityDao.getById(id)
     }
 
+    /**
+     * Returns a LiveData object containing all the Community entities in the database.
+     *
+     * @return A LiveData object containing a List of Community entities.
+     */
     fun getAll(): LiveData<List<Community>> {
         return communityDao.getAll()
     }
 
+    /**
+     * Deletes the given Community entity from the database.
+     *
+     * @param community The Community entity to be deleted.
+     */
     fun delete(community: Community) {
         viewModelScope.launch {
             communityDao.delete(community)
         }
     }
 
+    /**
+     * Inserts the given Community entity into the database.
+     *
+     * @param community The Community entity to be inserted.
+     * @throws SQLiteConstraintException if there's a constraint violation.
+     */
     @Throws(SQLiteConstraintException::class)
     fun insert(community: Community) {
         viewModelScope.launch(IO) {
@@ -43,6 +79,11 @@ class CommunityViewModel(application: Application) : AndroidViewModel(applicatio
         }
     }
 
+    /**
+     * Factory class for creating CommunityViewModel instances.
+     *
+     * @param application The application instance.
+     */
     class CommunityFactory(
         private val application: Application,
     ) : ViewModelProvider.Factory {
