f55ad1fa1ae65c044268bf1a2c4fbdec1c01b85b
Léo Barroux <leo.barroux@gmail.com>
2023-02-28 12:04:37 +0100

    feat: documentation Utils.kt

--- app/src/main/java/fr/chatavion/client/util/Utils.kt
+++ app/src/main/java/fr/chatavion/client/util/Utils.kt
@@ -3,8 +3,16 @@ package fr.chatavion.client.util
 import android.content.Context
 import android.widget.Toast
 
+/**
+ * A utility class providing methods for displaying Toast messages.
+ */
 object Utils {
 
+    /**
+     * Shows a short duration Toast message with the given text.
+     * @param text the text to display in the Toast
+     * @param context the Context in which to display the Toast
+     */
     fun showInfoToast(text: String, context: Context) {
         Toast.makeText(
             context,
@@ -13,9 +21,15 @@ object Utils {
         ).show()
     }
 
-    fun showErrorToast(text: String, context: Context){
-        Toast.makeText(context,
-            "ERROR: "+text,
+    /**
+     * Shows a long duration Toast message with the given text preceded by "ERROR: ".
+     * @param text the text to display in the Toast
+     * @param context the Context in which to display the Toast
+     */
+    fun showErrorToast(text: String, context: Context) {
+        Toast.makeText(
+            context,
+            "ERROR: $text",
             Toast.LENGTH_LONG
         ).show()
     }
