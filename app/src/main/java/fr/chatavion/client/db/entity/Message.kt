a924911745140b2f0d24db9ccfc0e327baa21e5d
Léo Barroux <leo.barroux@gmail.com>
2023-02-28 11:47:05 +0100

    fix: Message documentation

--- app/src/main/java/fr/chatavion/client/db/entity/Message.kt
+++ app/src/main/java/fr/chatavion/client/db/entity/Message.kt
@@ -1,24 +1,18 @@
 package fr.chatavion.client.db.entity
 
 /**
-A data class representing a message sent or received in a chat application.
-- @property pseudo the pseudonym of the sender or receiver of the message.
-- @property message the content of the message.
-- @property status the status of the message, indicating whether it has been sent or received.
-- @property send a flag indicating whether the message has been sent by the user or not.
+ * A data class representing a message sent or received in a chat application.
  */
 data class Message(
     val pseudo: String,
     val message: String,
     val status: MessageStatus,
     var send: Boolean,
-    var sendRetry : Int,
+    var sendRetry: Int,
 )
 
 /**
-An enum class representing the status of a [Message].
-@property SEND the status indicating that the message has been sent.
-@property RECEIVED the status indicating that the message has been received.
+ * An enum class representing the status of a [Message].
  */
 enum class MessageStatus {
     SEND,
